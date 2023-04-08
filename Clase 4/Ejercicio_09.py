231e77e9f961091a3197badfcc8b2b4e0c8c117b
martinluque12 <m.ezequiel.luque@gmail.com>
2023-04-08 15:13:40 -0300

    Ejercicio 09 completo

--- Clase 4/Ejercicio_09.py
+++ Clase 4/Ejercicio_09.py
@@ -1,7 +1,7 @@
 '''
-1G MARTIN LUQUE
+1G MARTIN LUQUE 
 
-Ejercicio 08
+Ejercicio 09 Sin usar listas
 
 Una concesionaria de autos nos pide desarrollar un sistema para controlar el stock de autos
 que tienen disponible a la venta.
@@ -10,22 +10,45 @@ Para esto se necesita saber de cada auto:
 * año del modelo
 * el precio 
 (validar los tipos de datos ingresados) y mostrarlos por pantalla en forma secuencial y ordenada. Realizar el ejercicio
-sin usar listas primero, y despues usando listas y comparar la composición del código.
+sin usar listas primero, y después usando listas y comparar la composición del código.
 '''
+año_actual = 2023
 
-mensaje = "HOLA! Nos alegra que nos hayas elegido para comprar/cambiar tu auto.\n"
-mensaje += "Elige una opcion.\n1-Marca del auto. \n2-Modelo del auto (Año del auto) \n3-Precio del auto."
-opcion_elegida = input(mensaje)
+información_autos = ""
 
-while True:
-    
-    if opcion_elegida == "1":
-        marca_auto = input("\nMarca del auto que desea adquirir. \n>")
-        while marca == " " and type(marca) != type(str()):
-            marca = input("Error! Elija una marca valida. \n>")
+respuesta = "s"
 
-    elif opcion_elegida == "2":
-        modelo_auto = input("\nModelo del auto. (Año de fabricación del auto) \n>")
-        while modelo_auto == " " and len(modelo_auto) < 4 and len(modelo_auto) > 4 :
-            modelo_auto = input("Error! Ingrese un modelo valido ")
+while respuesta == "s":
 
+
+    marca_auto = input("\nIngrese la marca del auto : > ")
+    while not marca_auto or marca_auto.isnumeric(): 
+        marca_auto = input("Erro! dato no valido. vuelva a ingresarlo. > ")
+
+    while True:
+        try:
+            modelo_auto = int(input("\nIngrese el modelo del auto. (Año de fabricación, 2008 en adelante) > "))
+            while modelo_auto < 2008 or modelo_auto > año_actual:
+                modelo_auto = int(input("Error! Año incorrecto, vuelva a ingresarlo. > "))
+            break
+        except ValueError:
+            print("Error! Ingrese un dato numérico.")
+
+    while True:
+        try:
+            precio_auto = int(input("\nPrecio del auto (En pesos) > "))
+            while precio_auto < 1500000:
+                precio_auto = int(input("Error! Precio invalido. Vuelva a ingresar un precio valido. >"))
+            break
+        except ValueError:
+            print("Error! Ingrese un dato numérico.")
+
+    información_autos += "\n\nMarca del auto: {0} | Modelo del auto: {1} | Precio del auto: ${2:,.0f}".format(marca_auto, 
+                                                                                                          modelo_auto, 
+                                                                                                          precio_auto)
+
+    respuesta = input('¿Quiere seguir ingresando? \nPresione "S" para si o "N" para no. > ').lower()
+    while respuesta != "s" and respuesta != "n":
+        respuesta = input('Error! Ingrese una opción valida "S" o "N" > ').lower()
+
+print(información_autos)
---
+++ Clase 4/Ejercicio_10.py
@@ -0,0 +1,24 @@
+from datetime import date
+from datetime import datetime
+'''
+1G MARTIN LUQUE 
+
+Ejercicio 09 Pero con listas
+
+Una concesionaria de autos nos pide desarrollar un sistema para controlar el stock de autos
+que tienen disponible a la venta.
+Para esto se necesita saber de cada auto: 
+* la marca
+* año del modelo
+* el precio 
+(validar los tipos de datos ingresados) y mostrarlos por pantalla en forma secuencial y ordenada. Realizar el ejercicio
+sin usar listas primero, y después usando listas y comparar la composición del código.
+'''
+año_actual = date.today()
+
+respuesta = "s"
+
+while respuesta == "s":
+    pass
+
+print(año_actual)
