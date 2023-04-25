78324b2480704204b26fc7959febb3745c90ac86
martinluque12 <m.ezequiel.luque@gmail.com>
2023-04-25 00:23:36 -0300

    actualizo

---
+++ Clase 9/cadenas.py
@@ -0,0 +1,13 @@
+#print recibe un espacio en memoria crea un espacio en memoria para un objeto de str, guarda el espacio justo de lo que se muestra
+#print lee lo que hay en esa dirección de memoria y lo muestra 
+print("Hola mundo")
+
+x = "Hola mundo"
+#lower convierte todos los caracteres a minuscula
+x.lower()
+#es lo mismo
+"Chau mundo".lower()
+#pasa todo a mayuscula
+x.upper()
+#convierte a x en una lista de los caracteres que contiene
+aux = list(x)
